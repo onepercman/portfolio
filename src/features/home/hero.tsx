c0e5275a9159d0a1ce0fcc554a82a27b66a6bc05
onepercman ‎ <onepercman@onepercmans-MacBook-Pro.local>
2024-09-16 22:56:22 +0700

    🚀 update hero

--- src/features/home/hero.tsx
+++ src/features/home/hero.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/libs/atoms"
-import { motion, useScroll, useSpring, useTransform } from "framer-motion"
+import { motion, useScroll, useTransform } from "framer-motion"
 import { FC } from "react"
 import { LuExternalLink, LuGithub, LuLinkedin, LuMail, LuSmartphone } from "react-icons/lu"
 import { contact } from "./data"
@@ -7,11 +7,15 @@ import { contact } from "./data"
 export const Hero: FC = () => {
   const { scrollYProgress } = useScroll()
 
-  const rotate = useSpring(useTransform(scrollYProgress, [0, 1], ["0", "720deg"]))
-  const translateX = useSpring(useTransform(scrollYProgress, [0, 1], ["0", "100%"]))
+  const translateL = useTransform(scrollYProgress, [0, 1], ["0", "200%"])
+  const opacityDown = useTransform(scrollYProgress, [0, 0.2], ["100%", "0%"])
+  const translateYL = useTransform(scrollYProgress, [0, 1], ["0", "60vh"])
+  const translateR = useTransform(scrollYProgress, [0, 1], ["0", "150%"])
+  const scaleL = useTransform(scrollYProgress, [0, 1], ["100%", "500%"])
+  const scaleR = useTransform(scrollYProgress, [0, 1], ["100%", "0%"])
 
   return (
-    <motion.section className="relative grid min-h-screen grid-cols-1 items-center gap-4 p-6 sm:grid-cols-2 sm:p-8">
+    <motion.section className="relative grid min-h-screen grid-cols-1 items-center gap-4 overflow-hidden p-6 sm:grid-cols-2 sm:p-8">
       <motion.div
         initial={{ translateX: "-100%" }}
         animate={{ translateX: "0" }}
@@ -20,41 +24,75 @@ export const Hero: FC = () => {
           stiffness: 260,
           damping: 20,
         }}
+        style={{}}
         className="relative flex h-full w-full flex-col items-start justify-center gap-8"
       >
         <div className="flex flex-col text-4xl font-semibold uppercase sm:text-6xl">
-          <span>My name</span>
-          <span className="text-2xl sm:text-4xl">is</span>
-          <span className="text-transparent [-webkit-text-stroke:_2px_#ef4444]">onepercman</span>
+          <motion.span style={{ opacity: opacityDown }}>My name</motion.span>
+          <motion.span style={{ opacity: opacityDown }} className="text-2xl sm:text-4xl">
+            is
+          </motion.span>
+          <motion.span
+            style={{ translateX: translateL, translateY: translateYL, scale: scaleL }}
+            className="text-transparent [-webkit-text-stroke:_2px_#ef4444]"
+          >
+            onepercman
+          </motion.span>
         </div>
-        <div className="text-lg sm:text-3xl">
+        <motion.div style={{ opacity: opacityDown }} className="text-lg sm:text-3xl">
           <span className="font-bold">Frontend Engineer</span> based in <span className="font-bold">VN</span>
-        </div>
-        <Button size="lg" color="primary" rightIcon={<LuExternalLink />}>
-          Work with me
-        </Button>
-        <div className="flex flex-col items-start gap-3">
-          <a target="_blank" href={`tel:${contact.phone}`} className="inline-flex items-center gap-2 whitespace-nowrap">
+        </motion.div>
+        <motion.div style={{ opacity: opacityDown }}>
+          <Button size="lg" color="primary" rightIcon={<LuExternalLink />}>
+            Work with me
+          </Button>
+        </motion.div>
+        <motion.div style={{ opacity: opacityDown }} className="flex flex-col items-start gap-3">
+          <motion.a
+            initial={{ translateX: "10rem", opacity: 0 }}
+            animate={{ translateX: "0", opacity: 1 }}
+            transition={{ duration: 0.5, delay: 0.2 }}
+            target="_blank"
+            href={`tel:${contact.phone}`}
+            className="inline-flex items-center gap-2 whitespace-nowrap"
+          >
             <LuSmartphone />
             <span>{contact.phone}</span>
-          </a>
-          <a
+          </motion.a>
+          <motion.a
+            initial={{ translateX: "10rem", opacity: 0 }}
+            animate={{ translateX: "0", opacity: 1 }}
+            transition={{ duration: 0.5, delay: 0.4 }}
             target="_blank"
             href={`mailto:${contact.email}`}
             className="inline-flex items-center gap-2 whitespace-nowrap"
           >
             <LuMail />
             <span>{contact.email}</span>
-          </a>
-          <a target="_blank" href={contact.github} className="inline-flex items-center gap-2 whitespace-nowrap">
+          </motion.a>
+          <motion.a
+            initial={{ translateX: "10rem", opacity: 0 }}
+            animate={{ translateX: "0", opacity: 1 }}
+            transition={{ duration: 0.5, delay: 0.6 }}
+            target="_blank"
+            href={contact.github}
+            className="inline-flex items-center gap-2 whitespace-nowrap"
+          >
             <LuGithub />
             <span>@onepercman</span>
-          </a>
-          <a target="_blank" href={contact.linkedin} className="inline-flex items-center gap-2 whitespace-nowrap">
+          </motion.a>
+          <motion.a
+            initial={{ translateX: "10rem", opacity: 0 }}
+            animate={{ translateX: "0", opacity: 1 }}
+            transition={{ duration: 0.5, delay: 0.8 }}
+            target="_blank"
+            href={contact.linkedin}
+            className="inline-flex items-center gap-2 whitespace-nowrap"
+          >
             <LuLinkedin />
             <span>@onepercman</span>
-          </a>
-        </div>
+          </motion.a>
+        </motion.div>
       </motion.div>
       <motion.div
         initial={{ translateY: "-100%" }}
@@ -64,10 +102,10 @@ export const Hero: FC = () => {
           stiffness: 260,
           damping: 20,
         }}
-        style={{ translateX }}
+        style={{ translateY: translateR, scale: scaleR }}
         className="absolute bottom-0 right-0 -z-10 aspect-square w-full overflow-hidden rounded-t-full sm:relative sm:rounded-full"
       >
-        <motion.img src="/hero/myself.jpeg" className="h-full w-full rounded-t-full" style={{ rotate }} />
+        <motion.img src="/hero/myself.jpeg" className="h-full w-full rounded-t-full" />
       </motion.div>
     </motion.section>
   )

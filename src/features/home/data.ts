909f0973f7c9f1bb50da426f027a5694a197d787
onepercman ‎ <onepercman@onepercmans-MacBook-Pro.local>
2024-09-26 08:35:08 +0700

    🚀 update data

--- src/features/home/data.ts
+++ src/features/home/data.ts
@@ -2,7 +2,8 @@ export const contact = {
   phone: "+84 918 048 534",
   email: "onepercman@gmail.com",
   github: "https://github.com/onepercman",
-  linkedin: "https://www.linkedin.com/in/onepercman",
+  linkedin: "https://linkedin.com/in/onepercman",
+  telegram: "https://t.me/onepercman",
 }
 
 export const experience = [
